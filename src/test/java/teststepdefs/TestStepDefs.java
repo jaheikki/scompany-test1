f1649ff0f18c174c841ed8c8de5bb3557fdbe44c
Heikkila Jari H <jari.h.heikkila@tieto.com>
2018-08-22 19:00:36 +0300

    Add Given step to delete old file

--- src/test/java/teststepdefs/TestStepDefs.java
+++ src/test/java/teststepdefs/TestStepDefs.java
@@ -1,11 +1,14 @@
 package teststepdefs;
 
 import common.CommonVars;
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -13,6 +16,23 @@ import static common.Common.printMethodName;
 
 
 public class TestStepDefs {
+
+    @Given("^test file (.*) should not exist$")
+    public void testFileShouldNotExist(String filename) {
+        printMethodName();
+
+        Path path = Paths.get(filename);
+        try {
+            Files.delete(path);
+            System.out.println("File "+filename+ " from previous run deleted successfully.");
+        } catch (IOException e) {
+            if (!Files.exists(path)) {
+                System.out.println("File "+filename+ " from previous run does not exist, ok.");
+            } else {
+                throw new RuntimeException("Failed to delete "+filename+" from previous run!");
+            }
+        }
+    }
 
     @When("^i print numbers (.*) to (.*) to file (.*)$")
     public void iWriteNumbersToFile(int firstNumber,int lastNumber, String filename) {
@@ -36,7 +56,7 @@ public class TestStepDefs {
         printMethodName();
 
         try {
-            Files.lines(Paths.get(filename)).limit(lastNumber/ CommonVars.numberRangeDivisionFactor).forEach(System.out::println);
+            Files.lines(Paths.get(filename)).limit(lastNumber / CommonVars.numberRangeDivisionFactor).forEach(System.out::println);
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/test/resources/acceptancetests/number-handling.feature
+++ src/test/resources/acceptancetests/number-handling.feature
@@ -4,6 +4,7 @@ Feature: Java tests
 
   @numbers_1
   Scenario: print numbers to file
+    Given test file numbers.txt should not exist
     When i print numbers 1 to 100 to file numbers.txt
     Then i print half of the numbers from file numbers.txt having 100 as last number
 
